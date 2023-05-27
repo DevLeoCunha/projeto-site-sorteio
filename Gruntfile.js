f193264f74c787561289807be9f34e6854f0d13c
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-05-26 21:37:25 -0300

    .

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css':'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less:{
+                files: ['src/styles/**/*.less'],
+                tasks: ['']
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production']);
+} 
